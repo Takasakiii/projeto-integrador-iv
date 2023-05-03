05fe38b89edcbd18f6aa6d86e9143e41f90ae993
Takasakiii <lucasmc2709@live.com>
2023-05-03 18:46:35 -0300

    Add: cors

---
+++ JobsApi/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace JobsApi.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static void UseAppCors(this WebApplication application)
+    {
+        application.UseCors(x => x
+            .AllowAnyHeader()
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .WithExposedHeaders("x-total-items")
+        );
+    }
+}
